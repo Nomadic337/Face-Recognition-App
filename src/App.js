21f6dc7389a839d415db753c438d3dbd328a1e85
Wolf S Laleau <devlaleau@gmail.com>
2020-06-24 13:00:47 -0400

    Image Component and basic functionality setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
+import FaceRecognition from './Components/FaceRecognition/FaceRegnition';
+import Clarifai from 'clarifai';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 import Particles from 'react-particles-js';
 import './App.css';
+
+
+const app = new Clarifai.App({
+ apiKey: '52178bdd052b404bacc099f514b720ec'
+});
 
 const ParticlesOptions = {
     particles: {
@@ -24,15 +31,29 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   }
 
   onButtonSubmit = () => {
-    console.log('Click');
+    this.setState({imageUrl: this.state.input});
+    // console.log('Click');
+    app.models
+    .predict(
+      Clarifai.COLOR_MODEL,
+      "https://samples.clarifai.com/face-det.jpg")
+    .then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
 
   render() {
@@ -45,8 +66,9 @@ class App extends Component {
         <Rank />
         <ImageLinkForm 
         onInputChange={this.onInputChange} 
-        onButtonSubmit={this.onButtonSubmit} />
-        {/* <FaceRecognition /> */}
+        onButtonSubmit={this.onButtonSubmit} 
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
   
       </div>
     );
---
+++ src/Components/FaceRecognition/FaceRegnition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FacialRecognition = () => {
+    return (
+        <div className='center'>
+            <img src="https://samples.clarifai.com/face-det.jpg" alt=""/>
+        </div>
+    )
+
+}
+
+export default FacialRecognition;
