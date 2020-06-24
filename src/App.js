847735568606fcbc2bc53c935eb23d9bf3fb3a50
Wolf S Laleau <devlaleau@gmail.com>
2020-06-24 13:32:24 -0400

    Bounding_box values returns work

--- src/App.js
+++ src/App.js
@@ -44,11 +44,11 @@ class App extends Component {
     // console.log('Click');
     app.models
     .predict(
-      Clarifai.COLOR_MODEL,
-      "https://samples.clarifai.com/face-det.jpg")
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
     .then(
     function(response) {
-      console.log(response);
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
     },
     function(err) {
       // there was an error
